bb4cfcf4700ae245b9a9cdae29a3ba3871e3cf4a
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-05-07 09:18:08 -0600

    was going to add search but didnt need to

---
+++ react-weather/src/WeatherIndex/CitySearch.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class CitySearch extends Component {
+  constructor(){
+    super()
+    this.state = {
+      search: ''
+    }
+  }
+  handleChange = (e) => {
+    this.set({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefaul()
+    this.props.searchCity(this.state)
+  }
+  render(){
+    return <form onSubmit={this.handleSubmit}>
+    Type In the City <input onChange = {this.handleChange} type='text' name='search' />
+    <input type='submit' />
+    </form>
+  }
+}
+
+
+
+export default CitySearch
--- react-weather/src/WeatherIndex/WeatherIndex.jsx
+++ react-weather/src/WeatherIndex/WeatherIndex.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import LoginForm from '../LoginForm/LoginForm'
+// import CitySearch from '../WeatherIndex/CitySearch'
+
 
 class WeatherIndex extends Component {
   constructor(){
@@ -13,7 +15,7 @@ class WeatherIndex extends Component {
   }
   componentDidMount(){
     // this.searchWeather({search:''})
-    this.searchWeather("Denver")
+    this.searchWeather('Denver')
   }
   searchWeather = (formData) => {
     const searchURL = `https://api.openweathermap.org/data/2.5/weather?q=${formData}&APPID=5a3006b50c75c299630fcd16450bfbed`
@@ -43,7 +45,7 @@ class WeatherIndex extends Component {
     return (<div>
             <h1>Today</h1>
             <p>{this.state.cityName}</p>
-            
+
             <p>{this.state.main.humidity}</p>
             <p>{this.state.main.temp}</p>
             <p>{this.state.main.temp_max}</p>
